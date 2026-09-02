c7863c0315acc52b02e1886808b2bf2c46d96198
yko79135 <yko79135@gmail.com>
2026-09-02 14:55:35 +0900

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,12 @@
-import { CONFIG, send } from './_lib.js';
+import { CONFIG, PERIODS, send } from './_lib.js';
+import { calendarConfigured, emailConfigured } from './_integrations.js';
 
 export default function handler(req, res) {
   send(res, 200, {
     googleClientId: CONFIG.googleClientId,
     configured: Boolean(CONFIG.googleClientId),
+    periods: PERIODS,
+    calendar: calendarConfigured(),
+    email: emailConfigured(),
   });
 }
